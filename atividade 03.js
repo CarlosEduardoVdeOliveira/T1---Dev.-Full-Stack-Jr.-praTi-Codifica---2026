9a55f17a3caad63e0986002fb5e1c9da46547c78
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-05-15 11:49:32 -0300

    implementa programa para calcular o novo salário com base no percentual de aumento

--- atividade 03.js
+++ atividade 03.js
@@ -1,7 +1,22 @@
-
 /*
 3. Escreva um programa que pergunte o salário mensal de um funcionário e o
 percentual de aumento concedido pela empresa. Caso o salário seja menor que
 R$ 1.500,00, o percentual de aumento deve ser dobrado automaticamente. Exiba o
 novo salário do funcionário.
 */
+const prompt = require("prompt-sync")();
+
+const monthlySalary = Number(prompt("Qual atual salário: "));
+const percentageIncrease = Number(
+  prompt("Qual percentual de aumento (ex: 5, 10, 20): "),
+);
+
+function calculateTheNewSalary(monthlySalary, percentageIncrease) {
+  let convertToPercentage = percentageIncrease / 100;
+  if (monthlySalary <= 1500) {
+    convertToPercentage *= 2;
+  }
+  let newSalary = monthlySalary + monthlySalary * convertToPercentage;
+  return newSalary;
+}
+console.log(calculateTheNewSalary(monthlySalary, percentageIncrease));
