f23ae47d27687df72544e4c05b376f9816e8b4d3
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-05-23 13:14:24 -0300

    refatora a estrutura do código da tabuada e melhora a legibilidade dos comentários

--- atividade_02/atividade 01.js
+++ atividade_02/atividade 01.js
@@ -1,5 +1,24 @@
 /*
-1. Peça ao usuário um número e exiba sua tabuada completa (de 1 a 10) usando um
-laço for. Em seguida, pergunte se ele deseja ver outra tabuada e repita enquanto
-a resposta for "sim".
+1. Peça ao usuário um número e exiba sua tabuada completa (de 1 a 10)
+usando um laço for. Em seguida, pergunte se ele deseja ver outra 
+tabuada e repita enquanto a resposta for "sim".
 */
+const prompt = require("prompt-sync")()
+let number = Number(prompt("Digite um número: "))
+
+function multiplicationTable(number){
+  for (let i = 1; i <= 10; i++) {
+    let result = i * number
+    console.log(`${i} * ${number} = ${result}`)
+  }
+}
+let  toContinue = "sim"
+while(toContinue === "sim"){
+  multiplicationTable(number);
+  toContinue = prompt("Deseja ver outra tabuada? (sim ou não)").toLowerCase()
+  if (toContinue === "sim") {
+    number = Number(prompt("Digite um número: "))
+  }
+}
+
+
