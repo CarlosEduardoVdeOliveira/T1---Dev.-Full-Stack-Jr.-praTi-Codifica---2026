3e5e347069f27b4a8963917a8fc52826624edec3
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-05-23 13:22:51 -0300

    adiciona lógica para contar dígitos de um número inteiro positivo

--- atividade_02/atividade 02.js
+++ atividade_02/atividade 02.js
@@ -1,4 +1,22 @@
 /*
-2. Leia um número inteiro positivo e, usando um laço while, calcule e exiba quantos
-dígitos ele possui. Trate o caso do número zero (que possui 1 dígito).
+2. Leia um número inteiro positivo e, usando um laço while, calcule 
+e exiba quantos dígitos ele possui. Trate o caso do número 
+zero (que possui 1 dígito).
 */
+
+const prompt = require("prompt-sync")()
+
+let number = Number(prompt("Digite um número inteiro positivo: "));
+
+let count = 0;
+
+if (number === 0) {
+  count = 1;
+} else {
+  while (number > 0) {
+    number = Math.trunc(number / 10);
+    count++;
+  }
+}
+
+console.log(`Quantidade de dígitos: ${count}`);
