f4b8b9b7734770d60151f9dfe417901bba4af0bb
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-05-28 14:48:32 -0300

    adiciona funcionalidade para inverter uma palavra e verificar se é um palíndromo

--- atividade_02/atividade 08.js
+++ atividade_02/atividade 08.js
@@ -1,7 +1,32 @@
-
 /*
 8. Leia uma palavra, armazene seus caracteres em um array e, 
 percorrendo-o de trás para frente com um laço for, monte a palavra
 invertida. Exiba a palavra original, a invertida e informe se ela
 é um palíndromo.
 */
+
+const prompt = require("prompt-sync")();
+
+const word = prompt("Digite uma palavra: ").toLowerCase();
+
+const arrayWord = [];
+for (const character of word) {
+  arrayWord.push(character);
+}
+console.log(arrayWord);
+
+let inverseWord = "";
+for (let i = arrayWord.length - 1; i >= 0; i--) {
+  inverseWord += arrayWord[i];
+}
+
+console.log(`Palavra original: ${word}`);
+console.log(`Palavra invertida: ${inverseWord}`);
+
+if (word === inverseWord) {
+  console.log(`A palavra ${word} é palíndroma.`);
+} else {
+  console.log(
+    `A palavra ${word} não é palíndroma, pois é diferente de ${inverseWord}`,
+  );
+}
