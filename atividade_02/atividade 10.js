48caf49893616ba9c21f8b7cfc1affdaba88c8ec
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-05-29 11:36:16 -0300

    implementa cálculo da média de notas dos alunos e exibe o melhor desempenho

--- atividade_02/atividade 10.js
+++ atividade_02/atividade 10.js
@@ -3,3 +3,46 @@
 via laços aninhados. Calcule e exiba a média de cada aluno, a média
 geral da turma e qual aluno teve o melhor desempenho.
 */
+
+const classStudents = [
+  ["Lucas", 5, 7, 8, 7.8],
+  ["Yan", 8, 7, 9, 8.7],
+  ["João", 8.5, 8.8, 9, 9.3],
+];
+
+let totalNotes = 0;
+let totalValues = 0;
+
+let bestStudent = "";
+let bestAverage = 0;
+
+for (let i = 0; i < classStudents.length; i++) {
+
+  let sumStudent = 0;
+
+  for (let j = 1; j < classStudents[i].length; j++) {
+    sumStudent += classStudents[i][j];
+
+    totalValues += classStudents[i][j];
+    totalNotes++;
+  }
+
+  let averageStudent = sumStudent / 4;
+
+  console.log(
+    `Média do aluno ${classStudents[i][0]}: ${averageStudent.toFixed(2)}`
+  );
+
+  if (averageStudent > bestAverage) {
+    bestAverage = averageStudent;
+    bestStudent = classStudents[i][0];
+  }
+}
+
+let classAverage = totalValues / totalNotes;
+
+console.log(`\nMédia geral da turma: ${classAverage.toFixed(2)}`);
+
+console.log(
+  `Melhor aluno: ${bestStudent} (${bestAverage.toFixed(2)})`
+);
