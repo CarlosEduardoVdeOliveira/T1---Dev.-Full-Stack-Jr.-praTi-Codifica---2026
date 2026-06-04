7e5e3f249e3c461a5025d439468f9095d17d52a2
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-06-04 13:13:09 -0300

    implementa fila para simular atendimento em clínica com funções de chegada e atendimento de pacientes

--- atividade_03/atividade 11.js
+++ atividade_03/atividade 11.js
@@ -1,4 +1,3 @@
-
 /*
 11. Implemente uma fila usando um array para simular o atendimento de 
 uma clínica. Crie as funções chegarPaciente(nome) (enqueue), 
@@ -6,3 +5,51 @@ chamarProximo() (dequeue) e exibirFila(). Simule a chegada de 5
 pacientes e o atendimento de 3, exibindo o estado da fila a cada operação
 .*/
 
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(nome) {
+    this.items.push(nome);
+  }
+
+  dequeue() {
+    return this.items.shift() ?? null;
+  }
+
+  exibirFila() {
+    console.log("Fila:", this.items);
+  }
+}
+
+const fila = new Queue();
+
+function chegarPaciente(nome) {
+  fila.enqueue(nome);
+  console.log(`${nome} chegou.`);
+  fila.exibirFila();
+}
+
+function chamarProximo() {
+  const paciente = fila.dequeue();
+
+  if (paciente) {
+    console.log(`Chamando: ${paciente}`);
+  } else {
+    console.log("Não há pacientes na fila.");
+  }
+
+  fila.exibirFila();
+}
+
+
+chegarPaciente("Douglas");
+chegarPaciente("Jean");
+chegarPaciente("Lucas");
+chegarPaciente("Daniela");
+chegarPaciente("Eduarda");
+
+chamarProximo();
+chamarProximo();
+chamarProximo();
